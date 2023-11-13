8bd9426f7a958b5da6f08aeaec6bdae69309e448
marckAnder <138138163+marckAnder@users.noreply.github.com>
2023-11-13 17:52:49 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const weather = {
+    city: "City",
+    temp: "25° 19°",
+    desc: "Partly cloudy",
+};
+
+const cityElement = document.querySelector("weather__city");
+const tempElement = document.querySelector("weather__temp");
+const descElement = document.querySelector("weather__desc")
